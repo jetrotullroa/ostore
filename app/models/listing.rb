f31e604aac4d66d186bfedc231aaa770186a3bbb
Jetro Tull J Roa <jetrotullroa@gmail.com>
2015-09-02 15:56:04 +0800

    link listings to user and add user permission for adding editing and destroying a listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,7 @@ class Listing < ActiveRecord::Base
   if
     has_attached_file :image, :styles => { :medium => "200x>",
       :thumb => "100x100>" }, :default_url => "noimage.jpg"
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   else
     has_attached_file :image, :styles => { :medium => "200x>",
       :thumb => "100x100>" }, :default_url => "noimage.jpg",
@@ -15,4 +16,5 @@ class Listing < ActiveRecord::Base
   validates :price, numericality: { greater_than: 0 }
   validates_attachment_presence :image
 
+  belongs_to :user
 end
